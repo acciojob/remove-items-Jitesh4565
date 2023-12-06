8bbdd7fa807d13dc3d513ba20b44346617708de6
Jitesh4565 <73875204+Jitesh4565@users.noreply.github.com>
2023-12-06 17:34:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function removeColor(){
+	const options=document.getElementById('colorSelect');
+	const color=options.selectedIndex;
+	options.remove(color);
+	
+}
